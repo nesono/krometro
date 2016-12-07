ebf44e2da2e789e13e77ae2bdc6bf23d14f65abf
Jochen Issing <c.333+git@nesono.com>
2016-12-07 22:29:08 +0100

    add another benchmark (slower std dev calculation)

--- main.cpp
+++ main.cpp
@@ -2,14 +2,43 @@
 #include <cstdlib>
 #include "krometro/v1/krometro.h"
 
-KROM_METER(statistics, stddev, 10, 10)
+KROM_METER(statistics, stddev, 100, 10)
 {
     std::vector<int32_t> testvec(1000);
     for(auto i : testvec)
     {
         i = rand();
     }
-    auto result = krom::standarddeviation(testvec);
+    auto result = krom::internal::standarddeviation(testvec);
+}
+
+template<class T>
+double slow_stddev(const std::vector<T> &observations)
+{
+    double e_x = 0;
+    for (auto i : observations) {
+        e_x += i;
+    }
+    auto N = observations.size();
+    e_x /= N;
+    double res = 0;
+    for (auto i : observations) {
+        auto tmp = i - e_x;
+        res += tmp * tmp;
+    }
+
+    auto variance = res / (N - 1);
+    return std::sqrt(variance);
+}
+
+KROM_METER(statistics, stddev_slow, 100, 10)
+{
+    std::vector<int32_t> testvec(1000);
+    for(auto i : testvec)
+    {
+        i = rand();
+    }
+    auto result = slow_stddev(testvec);
 }
 
 int main() {

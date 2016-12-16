e2ec88d9e1bae2dea9552974ba3e693912ddcd79
Jochen Issing <c.333+git@nesono.com>
2016-12-16 23:47:55 +0100

    some fixes for conan - still not fully there

--- conanfile.py
+++ conanfile.py
@@ -1,20 +1,17 @@
 from conans import ConanFile, CMake
 
 class KrometroConan(ConanFile):
-   name = "krometro"
-   version = "0.1"
-   license = "BSD-new"
-   url = "https://github.com/nesono/krometro.git"
-   settings = "os", "compiler", "build_type", "arch"
-   requires = "gtest/1.8.0@lasote/stable"
-   generators = "cmake", "txt", "env"
-   default_options = "gtest:shared=False"
+    name = "krometro"
+    version = "0.1"
+    license = "BSD-new"
+    url = "https://github.com/nesono/krometro.git"
+    requires = "gtest/1.8.0@lasote/stable"
+    generators = "cmake", "txt", "env"
+    default_options = "gtest:shared=False"
+    exports = "include/krometro/v1/krometro.h"
+    build_policy = "missing"
 
-   def imports(self):
-      self.copy("*.h", dst="include/krometro/v1", src="krometro/v1")
-      self.copy("*.cpp", dst="test", src=".")
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin") # From bin to bin
+        self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
 
-   def build(self):
-      cmake = CMake(self.settings)
-      self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-      self.run('cmake --build . %s' % cmake.build_config)

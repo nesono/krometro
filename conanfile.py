0bbdc18def31a56e50fc9c758a8c2d31e844f36d
iss <jochen.issing@iis.fraunhofer.de>
2016-12-22 08:53:28 +0100

    move gtest dependency to dev scope

--- conanfile.py
+++ conanfile.py
@@ -5,11 +5,14 @@ class KrometroConan(ConanFile):
     version = "0.1"
     license = "BSD-new"
     url = "https://github.com/nesono/krometro.git"
-    requires = "gtest/1.8.0@lasote/stable"
     generators = "cmake", "txt", "env"
     default_options = "gtest:shared=False"
     exports = "include/krometro/v1/krometro.hpp", "include/krometro/v1/krometro_main.hpp"
     build_policy = "missing"
+
+    def configure(self):
+        if self.scope.dev:
+            self.requires("gtest/1.8.0@lasote/stable")
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin") # From bin to bin
